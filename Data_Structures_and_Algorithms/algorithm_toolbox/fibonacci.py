589db7945b150c2460eff5b5c181f180f199fc65
donavinlo <46114229+donavinlo@users.noreply.github.com>
2020-01-12 23:26:33 -0600

    Update fibonacci.py

--- Data_Structures_and_Algorithms/algorithm_toolbox/fibonacci.py
+++ Data_Structures_and_Algorithms/algorithm_toolbox/fibonacci.py
@@ -1,10 +1,5 @@
 # # Uses python3
 import numpy as np
-# def calc_fib(n):
-#     if (n <= 1):
-#         return n
-#
-#     return calc_fib(n - 1) + calc_fib(n - 2)
 
 def FibList(n):
     arr = np.zeros(n+1)
