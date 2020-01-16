fd900ab42359dfd584dcd27b81598754f85c0183
donavinlo <46114229+donavinlo@users.noreply.github.com>
2020-01-15 20:50:56 -0600

    Update majority_element.py

--- Data_Structures_and_Algorithms/algorithm_toolbox/majority_element.py
+++ Data_Structures_and_Algorithms/algorithm_toolbox/majority_element.py
@@ -1,43 +1,41 @@
 # Uses python3
 import sys
 
+# Find the majority of the list using divide and conquer (MergeSort used in this example)
 def get_majority_element(a, left, right):
-    n = len(a)
-    a.sort()
+    #Base Cases
     if left == right:
-        return -1
+        return a[left]
     if left + 1 == right:
         return a[left]
-    #write your code here
-    for i in range(right):
-        if
-
-
+    m = (left + right - 1) // 2
+    
+    #Run recursively to get left majority and right majority
+    leftm = get_majority_element(a, left, m + 1)
+    rightm = get_majority_element(a, m + 1, right)
+    
+    #Get count of left majority in full string
+    left_count = 0
+    for i in range(left, right):
+        if a[i] == leftm:
+            left_count += 1
+    if left_count > (right - left) // 2:
+        return leftm
+    
+    Get  count of right majority in full string
+    right_count = 0
+    for i in range(left, right):
+        if a[i] == rightm:
+            right_count += 1
+    if right_count > (right - left) // 2:
+        return rightm
     return -1
-
-
-
-# def MergeSort(a, left, right):
-#     m = n // 2
-#     if len(a) == 1:
-#         return a
-#     b = MergeSort(a[1:m +1])
-#     c = MergeSort(a[m+1:])
-#     a_prime = merge(b,c)
-#     return a_prime
-#
-# def Merge(b,c):
-#     d = []
-#     for i in range(len(b)+len(q)):
-#         if
-
-
 
 
 if __name__ == '__main__':
     input = sys.stdin.read()
     n, *a = list(map(int, input.split()))
-    if mergeSort(a) != -1:
+    if get_majority_element(a, 0, n) != -1:
         print(1)
     else:
         print(0)
