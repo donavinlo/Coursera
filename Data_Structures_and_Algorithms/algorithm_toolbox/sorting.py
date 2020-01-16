9390dad354ecfea75aeda577b874dd698f662558
donavinlo <46114229+donavinlo@users.noreply.github.com>
2020-01-15 23:05:11 -0600

    Add files via upload

---
+++ Data_Structures_and_Algorithms/algorithm_toolbox/sorting.py
@@ -0,0 +1,45 @@
+# Uses python3
+import sys
+import random
+
+def partition3(a, l, r):
+    x = a[l]
+    j = l
+    g = r
+    for i in range(l, r + 1):
+        if a[i] < x:
+            j += 1
+            a[i], a[j] = a[j], a[i]
+        elif a[i] > x:
+            a[g], a[i] = a[i], a[g]
+            g -= 1
+            i -= 1 # remain in the same i in this case
+    return j, g
+
+def partition2(a, l, r):
+    x = a[l]
+    j = l
+    for i in range(l + 1, r + 1):
+        if a[i] < x:
+            j += 1
+            a[i], a[j] = a[j], a[i]
+    return j, r
+
+
+def randomized_quick_sort(a, l, r):
+    if l >= r:
+        return
+    k = random.randint(l, r)
+    a[l], a[k] = a[k], a[l]
+    #use partition3
+    m1, m2 = partition3(a, l, r)
+    randomized_quick_sort(a, l, m1 - 1);
+    randomized_quick_sort(a, m2 + 1, r);
+
+
+if __name__ == '__main__':
+    input = sys.stdin.read()
+    n, *a = list(map(int, input.split()))
+    randomized_quick_sort(a, 0, n - 1)
+    for x in a:
+        print(x, end=' ')
