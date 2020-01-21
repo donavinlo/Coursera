2da7a76f9b3d0be0f669e32ca38b03783057a94f
donavinlo <46114229+donavinlo@users.noreply.github.com>
2020-01-21 08:57:42 -0600

    Update edit_distance.py

--- Data_Structures_and_Algorithms/algorithm_toolbox/dynamic_programming/edit_distance.py
+++ Data_Structures_and_Algorithms/algorithm_toolbox/dynamic_programming/edit_distance.py
@@ -8,16 +8,16 @@ def edit_distance(s, t, vals = None):
     if len(t) == 0:
         return len(s)
     if (len(s), len(t)) in vals:
-        return vals[len(s), len(t)] #same operation for mathc/mismatch but match you add 1
+        return vals[len(s), len(t)]
     if s[-1] != t[-1]:
         match = edit_distance(s[:-1], t[:-1], vals) + 1
     else:
         match= edit_distance(s[:-1], t[:-1], vals)
     deletion = edit_distance(s[:-1], t, vals) + 1
     insertion = edit_distance(s, t[:-1], vals) + 1
-    ans = min(match, deletion, insertion)
-    vals[len(s), len(t)] = ans
-    return ans
+    dist = min(match, deletion, insertion)
+    vals[len(s), len(t)] = dist
+    return dist
 
 if __name__ == "__main__":
     print(edit_distance(input(), input()))
